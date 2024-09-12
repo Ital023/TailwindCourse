e7d5ae4616321add990fd5d59e69fbf2cdd0e022
Ital023 <contadoitalo911@gmail.com>
2024-09-12 18:49:06 -0300

    feat: order

--- src/pages/Flexbox/index.tsx
+++ src/pages/Flexbox/index.tsx
@@ -11,10 +11,19 @@ export default function Flexbox() {
     // </div>
 
     //Ocupa o seu tamanho total possivel
-    <div className="flex bg-green-400 p-8">
-        <div className="flex-1 bg-green-800">1</div>
-        <div className="flex-1 bg-green-600">1</div>
-        <div className="flex-none bg-green-600">1</div>
-    </div>
+    // <div className="flex bg-green-400 p-8">
+    //     <div className="flex-1 bg-green-800">1</div>
+    //     <div className="flex-1 bg-green-600">1</div>
+    //     <div className="flex-none bg-green-600">1</div>
+    // </div>
+
+    //  <div className="flex flex-wrap bg-purple-300">
+    //    <div className="bg-purple-200 p-6 m-3 order-last">1</div>
+    //    <div className="bg-purple-300 p-6 m-3 order-5">2</div>
+    //    <div className="bg-purple-400 p-6 m-3 order-4">3</div>
+    //    <div className="bg-purple-500 p-6 m-3 order-3">4</div>
+    //    <div className="bg-purple-600 p-6 m-3 order-2">5</div>
+    //    <div className="bg-purple-700 p-6 m-3 order-first">6</div>
+    //  </div>
   );
 }
