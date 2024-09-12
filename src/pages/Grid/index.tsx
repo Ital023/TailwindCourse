93eef07c66449a2983d51a152c59a6c96bc92152
Ital023 <contadoitalo911@gmail.com>
2024-09-12 19:56:01 -0300

    feat: exercicio 8

--- src/pages/Grid/index.tsx
+++ src/pages/Grid/index.tsx
@@ -65,6 +65,22 @@ export default function Grid() {
         </div>
       </div>
 
+      {/* Exercicio 8 */}
+      <div className="grid grid-cols-4 m-2">
+        <div className="col-span-4 bg-yellow-500">Barra de navegação</div>
+        <div className="col-span-1 bg-blue-500">
+          <ul>
+            <li>Home</li>
+            <li>Sobre nos</li>
+            <li>Especialidades</li>
+          </ul>
+        </div>
+        <div className="col-span-3 bg-lime-500">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus tempore vel neque libero, mollitia soluta nobis est a? Sunt exercitationem tempora laudantium perferendis maxime saepe dolorem voluptas sed alias voluptates vitae iure deserunt eum dolores, debitis ab cum nemo ipsum magni perspiciatis. Illo iure nisi provident hic, cumque accusamus aliquid? Dolorum architecto accusantium repudiandae corrupti harum soluta, quisquam, neque odio cum error vel consequuntur laborum dolor deleniti, dignissimos maiores asperiores debitis commodi nulla! Magnam culpa, tempore ut odit tempora eum magni iste, consequuntur aliquam quod, quidem sunt hic id quae fugit ducimus nemo nam excepturi sint praesentium. Reprehenderit, veritatis perspiciatis consequuntur necessitatibus ullam architecto? Tempore beatae officia necessitatibus inventore veniam deserunt sit ipsa fugit! Saepe, nam temporibus, ipsa doloremque eaque harum autem iste sit fugiat omnis at voluptatem sequi sapiente eligendi. Magni accusantium quas ipsa et corporis in quidem recusandae iste nobis nihil autem veritatis, sapiente ea voluptatem dolor reiciendis beatae totam sunt. Dolor quisquam ratione et, rerum, quam sunt soluta repellendus optio accusamus consectetur qui perspiciatis totam est tempora quis facere odio cumque? Fugiat repellat nulla quia ab commodi similique nemo delectus placeat cupiditate sapiente. Tempore nisi maxime exercitationem nesciunt provident eius quae modi nobis aperiam ex incidunt doloremque nostrum ullam, excepturi laborum molestias, neque veniam repudiandae quas voluptatem ea omnis. Vitae repellat, eius repellendus asperiores maxime doloribus nesciunt officiis. Quibusdam itaque saepe libero. Iure at accusamus quo. Incidunt velit cum magnam, cupiditate quaerat earum consequuntur dicta nemo optio maiores. Adipisci tenetur, commodi ad neque dignissimos nobis similique consectetur impedit sit cumque cupiditate, dicta optio asperiores in. Optio dicta laudantium, necessitatibus nesciunt nobis quasi rem nisi incidunt qui architecto, cumque eum consequatur blanditiis. Minus laboriosam aliquam reprehenderit illum temporibus deleniti pariatur dicta accusamus aspernatur quaerat soluta quisquam magnam numquam modi itaque, optio repudiandae, dignissimos mollitia expedita illo? Reprehenderit dolor porro dignissimos optio odio corporis voluptas in impedit tempore maxime dolores quod voluptates enim, facilis libero. Nemo illum atque cumque error voluptas quis. Quasi vitae reprehenderit molestias laudantium, atque repellat perspiciatis voluptatibus nihil exercitationem quia consectetur commodi voluptates voluptatem doloremque ab deleniti qui quae rem maxime. Mollitia placeat eum est distinctio non labore, voluptatum, earum adipisci ex eius animi recusandae quae aspernatur exercitationem in, voluptatem iusto harum sint nulla repellendus neque autem. Sed possimus repellendus, explicabo corporis ipsum fugiat, consectetur, perspiciatis numquam quaerat cum ea! Esse ab maxime quisquam nihil nam ipsum animi ut nulla nesciunt, accusamus reiciendis sint pariatur.</p>
+        </div>
+
+      </div>
+
       
     </main>
   );
